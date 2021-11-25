7e7e76da8ec17a5417e6f2a4f6ed50d0499d5337
MusiKid <musikid@outlook.com>
2021-11-25 22:03:43 +0100

    chore: remove .class files

--- src/test/java/com/projet/parser/combinators/CombinatorsTest.java
+++ src/test/java/com/projet/parser/combinators/CombinatorsTest.java
@@ -1,5 +1,14 @@
 package com.projet.parser.combinators;
 
-public class CombinatorsTest {
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+public class CombinatorsTest {
+    @Test
+    @DisplayName("satisfy combinator")
+    void testSatisfy() throws ParseException {
+        final String testCase = "a";
+        assert(Combinators.satisfy(c -> c == 'a').apply(new State<>(testCase)));
+    }
 }
