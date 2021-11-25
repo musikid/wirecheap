5839038d9a6951aaba38b189834d8906933ae9fc
MusiKid <musikid@outlook.com>
2021-11-25 22:20:51 +0100

    test: improve tests

--- src/test/java/com/projet/parser/combinators/CombinatorsTest.java
+++ src/test/java/com/projet/parser/combinators/CombinatorsTest.java
@@ -1,14 +1,26 @@
 package com.projet.parser.combinators;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CombinatorsTest {
     @Test
     @DisplayName("satisfy combinator")
-    void testSatisfy() throws ParseException {
+    void testSatisfy() {
         final String testCase = "a";
-        assert(Combinators.satisfy(c -> c == 'a').apply(new State<>(testCase)));
+        assert (Combinators.satisfy(c -> c == 'a').apply(new State<>(testCase)));
+    }
+
+    @Test
+    @DisplayName("hexDigit combinator")
+    void testHexDigit() {
+        IntStream letters = IntStream.concat(IntStream.range('a', 'f' + 1), IntStream.range('a', 'F' + 1));
+        IntStream digits = IntStream.range('0', '9' + 1);
+        for (char digit : digits.mapToObj(c -> (char) c).collect(Collectors.toList())) {
+            assert (Combinators.hexDigit().apply(new State<>(digit + "")));
+        }
     }
 }
