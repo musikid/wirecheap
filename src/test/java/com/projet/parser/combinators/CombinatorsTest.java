3dfb916cde56f8cca7c749a162a63670ffa7d3d3
MusiKid <musikid@outlook.com>
2021-11-26 17:19:37 +0100

    test: add hexOffset test

--- src/test/java/com/projet/parser/combinators/CombinatorsTest.java
+++ src/test/java/com/projet/parser/combinators/CombinatorsTest.java
@@ -3,6 +3,7 @@ package com.projet.parser.combinators;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -23,8 +24,8 @@ public class CombinatorsTest {
     @Test
     @DisplayName("hexDigit combinator")
     void testHexDigit() {
-        IntStream digitRange = IntStream.range(0, 0x10);
-        for (char digit : digitRange.mapToObj(c -> Character.forDigit(c, 16)).collect(Collectors.toList())) {
+        IntStream digitRange = IntStream.rangeClosed(0, 0xF);
+        for (char digit : digitRange.mapToObj(i -> Character.forDigit(i, 16)).collect(Collectors.toList())) {
             final State<?> state = new State<>(digit + "");
             assertTrue(hexDigit().apply(state));
             assertEquals(state.getResult(), digit);
@@ -34,11 +35,23 @@ public class CombinatorsTest {
     @Test
     @DisplayName("hexByte combinator")
     void testHexByte() {
-        IntStream byteRange = IntStream.range(0, 0x100);
-        for (String byteString : byteRange.mapToObj(c -> String.format("%02x", c)).collect(Collectors.toList())) {
+        IntStream byteRange = IntStream.rangeClosed(0, 0xFF);
+        for (String byteString : byteRange.mapToObj(i -> String.format("%02x", i)).collect(Collectors.toList())) {
             final State<?> state = new State<>(byteString);
             assertTrue(hexByte().apply(state));
             assertEquals(state.getResult(), (byte) Integer.parseInt(byteString, 16));
+        }
+    }
+
+    @Test
+    @DisplayName("hexOffset combinator")
+    void testHexOffset() {
+        IntStream intRange = IntStream.range(0, 0xFFFF);
+        for (Iterator<String> it = intRange.mapToObj(i -> String.format("%02x", i)).iterator(); it.hasNext(); ) {
+            String offsetString = it.next();
+            final State<?> state = new State<>(offsetString);
+            assertTrue(hexOffset().apply(state));
+            assertEquals(state.getResult(), Integer.parseInt(offsetString, 16));
         }
     }
 }
