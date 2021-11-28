53bb476a7422dd5130b25309ee64c11cbe234f32
MusiKid <musikid@outlook.com>
2021-11-28 09:45:54 +0100

    test: improve tests

--- src/test/java/com/projet/parser/combinators/CombinatorTest.java
+++ src/test/java/com/projet/parser/combinators/CombinatorTest.java
@@ -22,6 +22,7 @@ public class CombinatorTest {
         assertEquals('a', testCase.getResult());
         assertEquals(2, testCase.checkpoint().pos);
 
+        // We want to ensure that the skip parser works correctly
         assertTrue(testCombinator.apply(testCase2));
         assertEquals('a', testCase2.getResult());
         assertEquals(1, testCase2.checkpoint().pos);
--- src/test/java/com/projet/parser/combinators/CombinatorsTest.java
+++ src/test/java/com/projet/parser/combinators/CombinatorsTest.java
@@ -9,53 +9,86 @@ import java.util.stream.IntStream;
 
 import static com.projet.parser.combinators.Combinators.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CombinatorsTest {
     @Test
     @DisplayName("satisfy combinator")
-    void testSatisfy() {
+    void testSatisfy() throws ParseException {
         final char testCase = 'a';
         final State<?> state = new State<>(testCase + "");
 
-        assertTrue(satisfy(c -> c == testCase).apply(state));
-        assertEquals(state.getResult(), testCase);
+        assertEquals(testCase, satisfy(c -> c == 'a').parse(state));
     }
 
     @Test
     @DisplayName("hexDigit combinator")
-    void testHexDigit() {
+    void testHexDigit() throws ParseException {
         IntStream digitRange = IntStream.rangeClosed(0, 0xF);
         for (char digit : digitRange.mapToObj(i -> Character.forDigit(i, 16)).collect(Collectors.toList())) {
-            final State<?> state = new State<>(digit + "");
+            assertEquals(digit, hexDigit().parse(digit + ""));
 
-            assertTrue(hexDigit().apply(state));
-            assertEquals(state.getResult(), digit);
+            // Test uppercase
+            if (Character.isAlphabetic(digit)) {
+                digit = Character.toUpperCase(digit);
+                assertEquals(digit, hexDigit().parse(digit + ""));
+            }
         }
     }
 
     @Test
     @DisplayName("hexByte combinator")
-    void testHexByte() {
+    void testHexByte() throws ParseException {
         IntStream byteRange = IntStream.rangeClosed(0, 0xFF);
         for (String byteString : byteRange.mapToObj(i -> String.format("%02x", i)).collect(Collectors.toList())) {
-            final State<?> state = new State<>(byteString);
-
-            assertTrue(hexByte().apply(state));
-            assertEquals(state.getResult(), (byte) Integer.parseInt(byteString, 16));
+            assertEquals((byte) Integer.parseInt(byteString, 16),
+                    hexByte().parse(byteString));
         }
     }
 
     @Test
     @DisplayName("hexOffset combinator")
-    void testHexOffset() {
+    void testHexOffset() throws ParseException {
         IntStream intRange = IntStream.range(0, 0xFFFF);
         for (Iterator<String> it = intRange.mapToObj(i -> String.format("%02x", i)).iterator(); it.hasNext(); ) {
             String offsetString = it.next();
-            final State<?> state = new State<>(offsetString);
-
-            assertTrue(hexOffset().apply(state));
-            assertEquals(state.getResult(), Integer.parseInt(offsetString, 16));
+            assertEquals(Integer.parseInt(offsetString, 16), hexOffset().parse(offsetString));
         }
+    }
+
+    @Test
+    @DisplayName("space combinator")
+    void testSpace() throws ParseException {
+        char space = ' ';
+        assertEquals(space, space().parse(space + ""));
+    }
+
+    @Test
+    @DisplayName("spaces combinator")
+    void testSpaces() throws ParseException {
+        String testStr = "         ";
+        State<String> state = new State<>(testStr);
+        assertEquals(testStr.chars().mapToObj(c -> (char) c).collect(Collectors.toList()), spaces().parse(state));
+        assertEquals(testStr.length(), state.checkpoint().pos);
+    }
+
+    @Test
+    @DisplayName("count combinator")
+    void testCount() throws ParseException {
+        String testStr = "aaa";
+        State<String> state = new State<>(testStr);
+        assertEquals(testStr.chars().mapToObj(c -> (char) c).collect(Collectors.toList()),
+                count(0, satisfy(c -> c == 'a')).parse(state));
+        assertEquals(testStr.length(), state.checkpoint().pos);
+
+        state = new State<>(testStr);
+        assertEquals(testStr.chars().mapToObj(c -> (char) c).collect(Collectors.toList()),
+                count(1, testStr.length(), satisfy(c -> c == 'a')).parse(state));
+        assertEquals(testStr.length(), state.checkpoint().pos);
+
+        state = new State<>("sts");
+        assertEquals(0,
+                count(0, testStr.length(), satisfy(c -> c == 'a')).parse(state).size());
+        assertThrows(ParseException.class, () -> count(2, satisfy(c -> c == 's')).parse(""));
     }
 }
